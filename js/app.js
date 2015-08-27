85c3ff5eb438c8d3b213489556997221dcb72921
weizheng1992 <740977067@qq.com>
2015-08-27 16:51:32 +0800

    updated the README

---
+++ js/app.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="angular1.3/angular.js" />
+/// <reference path="angular1.3/angular-route.js" />
+
+
+
+
+
+var app = angular.module('myApp', ['ngRoute']);
+
+app.controller('RootCtrl', ['$scope', function ($scope) {
+    $scope.title = "Home Page";
+}]);
+
+app.controller('CatsCtrl', ['$scope', function ($scope) {
+    $scope.title = "login Page";
+}]);
+app.controller('TestCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
+    console.log($routeParams.name);
+    $scope.title = $routeParams.name;
+}]);
+
+app.controller('AppCtrl', ['$scope', function ($scope) {
+    $scope.onViewLoad = function () {
+        console.log('view changed');
+    };
+}]);
---
+++ js/route.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="angular1.3/angular.js" />
+/// <reference path="angular1.3/angular-route.js" />
+
+
+
+
+var app = angular.module('myApp');
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'RootCtrl',
+            templateUrl: 'views/home.html'
+        })
+        .when('/login', {
+            controller: 'CatsCtrl',
+            templateUrl: 'views/login.html'
+        })
+        .when('/test/:name', {
+            controller: 'TestCtrl',
+            templateUrl: 'views/test.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+    $locationProvider.html5Mode(true);  //index.html  header 添加 base
+}]);
